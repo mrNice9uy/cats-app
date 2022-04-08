e56cf4b1de36762d945b7ff3f15c56a5ab111fe6
Vitalii Karamanov <vitalii_karamanov@epam.com>
2022-04-08 14:30:52 +0300

    Remove console.log

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -6,18 +6,15 @@ import {
   StyledCheckbox,
 } from "../styledComponents/StyledCheckboxes";
 
-const Checkbox = (props) => {
-  console.log(props);
-  return (
-    <CheckboxContainer>
-      <HiddenCheckbox {...props} />
-      <StyledCheckbox checked={props.checked}>
-        <Icon viewBox="0 0 24 54">
-          <polyline points="20 6 9 17 4 12" />
-        </Icon>
-      </StyledCheckbox>
-    </CheckboxContainer>
-  );
-};
+const Checkbox = (props) => (
+  <CheckboxContainer>
+    <HiddenCheckbox {...props} />
+    <StyledCheckbox checked={props.checked}>
+      <Icon viewBox="0 0 24 54">
+        <polyline points="20 6 9 17 4 12" />
+      </Icon>
+    </StyledCheckbox>
+  </CheckboxContainer>
+);
 
 export default Checkbox;
